8bbd6b3ed0a22d6baf4f2cc58f2c1cd649bcfe12
Ruben <rubenalb90@gmail.com>
2021-11-13 18:19:35 +0100

    Get the CORS origins from CORS_ORIGINS env variable

--- _api/app/rooms/middlewares/cors.ts
+++ _api/app/rooms/middlewares/cors.ts
@@ -3,6 +3,6 @@ import Cors from 'cors'
 export default function (httpMethod: string) {
   return Cors({
     methods: [httpMethod.toUpperCase(), 'HEAD'],
-    origin: process.env.NEXT_PUBLIC_APP_BASE_URL
+    origin: process.env.CORS_ORIGINS.split(',')
   })
 }
--- src/utils/browserEnv.ts
+++ src/utils/browserEnv.ts
@@ -1,3 +1,2 @@
-export const APP_BASE_URL = process.env.NEXT_PUBLIC_APP_BASE_URL
 export const PUSHER_KEY = process.env.NEXT_PUBLIC_PUSHER_KEY
 export const PUSHER_CLUSTER = process.env.NEXT_PUBLIC_PUSHER_CLUSTER
